6cfa05e0c71fcf37e43be88f02f901cfb5a30d80
kmendelev <kirill.mendelev@gmail.com>
2023-11-04 00:57:01 +0000

    Basic changes

--- Hello.py
+++ Hello.py
@@ -14,6 +14,7 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+import pandas as pd
 
 LOGGER = get_logger(__name__)
 
@@ -28,23 +29,31 @@ def run():
 
     st.sidebar.success("Select a demo above.")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+    df = pd.DataFrame(
+        {"col1": [1,2,3,4,5],
+        "col2": [1,2,3,4,5]},
+        
     )
+
+    df
+
+    # st.markdown(
+    #     """
+    #     Streamlit is an open-source app framework built specifically for
+    #     Machine Learning and Data Science projects.
+    #     **👈 Select a demo from the sidebar** to see some examples
+    #     of what Streamlit can do!
+    #     ### Want to learn more?
+    #     - Check out [streamlit.io](https://streamlit.io)
+    #     - Jump into our [documentation](https://docs.streamlit.io)
+    #     - Ask a question in our [community
+    #       forums](https://discuss.streamlit.io)
+    #     ### See more complex demos
+    #     - Use a neural net to [analyze the Udacity Self-driving Car Image
+    #       Dataset](https://github.com/streamlit/demo-self-driving)
+    #     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    # """
+    # )
 
 
 if __name__ == "__main__":
